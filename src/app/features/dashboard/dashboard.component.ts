a195700fb3e4f436abbcbc69b84ff4ec25dff73b
karl262 <carlins502@gmail.com>
2025-02-12 07:26:04 -0600

    Fix: Re-Design for dashboard.

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +9,17 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
   stats = [
     { title: 'Nuevos Pacientes', value: 150, icon: 'person_add' },
     { title: 'Tasa de Recaída', value: '53%', icon: 'bar_chart' },
     { title: 'Citas Registradas', value: 44, icon: 'event' },
     { title: 'Pacientes Activos', value: 65, icon: 'groups' }
   ];
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 
 }
